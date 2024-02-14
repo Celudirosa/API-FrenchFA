657368cd3ff19a98a015ca8b7ad7787975802c17
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-14 17:53:04 +0100

    Add email

---
+++ src/main/java/com/example/entities/Email.java
@@ -0,0 +1,34 @@
+package com.example.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "emails")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Email implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "The email cannot be empty")
+    private String email;
+    // TODO Checkear caracteres obligatorios (@blue.com)
+
+}
