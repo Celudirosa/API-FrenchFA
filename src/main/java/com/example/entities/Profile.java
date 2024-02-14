676c944d6863c053b8898f7570827d39f31dda78
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-14 18:10:04 +0100

    Add relacion (padre) Profile

--- src/main/java/com/example/entities/Profile.java
+++ src/main/java/com/example/entities/Profile.java
@@ -1,11 +1,17 @@
 package com.example.entities;
 
 import java.io.Serializable;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -30,4 +36,8 @@ public class Profile implements Serializable{
     @NotBlank(message = "The profile cannot be empty")
     private String profile;
 
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "profile")
+    @JsonIgnore
+    private List<Attendee> attendees;
+    
 }
