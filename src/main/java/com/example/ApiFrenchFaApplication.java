4b478a63095b5a194f3a9684aab6cab94e47a0e4
celudirosa <cluqued@live.com>
2024-02-14 16:36:34 +0100

    initial commit

---
+++ src/main/java/com/example/ApiFrenchFaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFrenchFaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFrenchFaApplication.class, args);
+	}
+
+}
