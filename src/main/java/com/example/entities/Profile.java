74b7b3cbbd03ac5fabac10474315afdec3a27b1a
lvarezisabel7 <maisabel2000@gmail.com>
2024-02-14 17:39:26 +0100

    Add anotaciones en Profile

--- src/main/java/com/example/entities/Profile.java
+++ src/main/java/com/example/entities/Profile.java
@@ -1,5 +1,10 @@
 package com.example.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "profiles")
 public enum Profile {
     INTERNAL, BOOTCAMP
 
