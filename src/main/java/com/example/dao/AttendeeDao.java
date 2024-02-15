a67a5dc6d04337c9e174790dcfc573917ff4b95b
celudirosa <cluqued@live.com>
2024-02-15 15:46:47 +0100

    add findAll metodo sin pageable

--- src/main/java/com/example/dao/AttendeeDao.java
+++ src/main/java/com/example/dao/AttendeeDao.java
@@ -1,7 +1,10 @@
 package com.example.dao;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,8 +14,13 @@ import com.example.entities.Attendee;
 @Repository
 public interface AttendeeDao extends JpaRepository<Attendee, Integer> {
 
+    // metodo para recuperar attendee paginados
     @Query(value = "SELECT p from Attendee p left join fetch p.profile", 
         countQuery = "select count(p) from Attendee p left join p.profile")
     public Page<Attendee> findAll(Pageable pageable);
+
+    // recuperar attendees ordenados sin paginacion
+    @Query(value = "SELECT p from Attendee p left join fetch p.profile")
+    public List<Attendee> findAll(Sort sort);
 
 }
