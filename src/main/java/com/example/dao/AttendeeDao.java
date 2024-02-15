6fbf20c1b8e554ada4bbed03ddb68b5ad99ce92a
celudirosa <cluqued@live.com>
2024-02-15 15:44:30 +0100

    add findAll metodo pageable

--- src/main/java/com/example/dao/AttendeeDao.java
+++ src/main/java/com/example/dao/AttendeeDao.java
@@ -1,11 +1,18 @@
 package com.example.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.entities.Attendee;
 
 @Repository
 public interface AttendeeDao extends JpaRepository<Attendee, Integer> {
+
+    @Query(value = "SELECT p from Attendee p left join fetch p.profile", 
+        countQuery = "select count(p) from Attendee p left join p.profile")
+    public Page<Attendee> findAll(Pageable pageable);
 
 }
