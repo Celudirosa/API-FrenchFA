6ffeb71c57744141a1c6bbc7acb16d9c94494a5b
AndreaSerge <acserger@unal.edu.co>
2024-02-15 17:37:07 +0100

    add AttendeeServiceImpl

---
+++ src/main/java/com/example/services/AttendeeServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.services;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.example.dao.AttendeeDao;
+import com.example.entities.Attendee;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AttendeeServiceImpl implements AttendeeService {
+    
+    private final AttendeeDao attendeeDao;
+
+    @Override
+    public Page<Attendee> findAll(Pageable pageable) {
+        return attendeeDao.findAll(pageable);
+    }
+
+    @Override
+    public List<Attendee> findAll(Sort sort) {
+        return attendeeDao.findAll(sort);
+    }
+
+    @Override
+    public List<Attendee> findAll() {
+        return attendeeDao.findAll();
+    }
+
+    @Override
+    public Attendee findById(int id) {
+        return attendeeDao.findById(id);
+    }
+
+    @Override
+    public Attendee save(Attendee attendee) {
+        return attendeeDao.save(attendee);
+    }
+
+    @Override
+    public void delete(Attendee attendee) {
+        attendeeDao.delete(attendee);
+    }
+
+}
