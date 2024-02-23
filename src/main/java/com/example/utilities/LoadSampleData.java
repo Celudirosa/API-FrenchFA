de8379aaa0bd1f094e509995518be0c7d01a3ef4
cotyarnau <cotyarnau@gmail.com>
2024-02-23 10:44:22 +0100

    Feedbacks conectados con Attendee

--- src/main/java/com/example/utilities/LoadSampleData.java
+++ src/main/java/com/example/utilities/LoadSampleData.java
@@ -37,55 +37,7 @@ public class LoadSampleData {
                     .profile("Internal")
                     .build());
 
-            // Feedbacks Bootcamper
-            List<Feedback> feedbacksBootcamper1 = new ArrayList<>();
-
-            Feedback feedback1Bootcamper1 = Feedback.builder()
-                    .Level(Level.C1)
-                    .date(LocalDate.of(2024, 1, 01))
-                    .comments("She speaks very well")
-                    .build();
-
-            Feedback feedback2Bootcamper1 = Feedback.builder()
-                    .Level(Level.C2)
-                    .date(LocalDate.of(2024, 2, 01))
-                    .comments("She has improved")
-                    .build();
-
-            feedbackService.save(feedback1Bootcamper1);
-            feedbackService.save(feedback2Bootcamper1);
-
-            feedbacksBootcamper1.add(feedback1Bootcamper1);
-            feedbacksBootcamper1.add(feedback2Bootcamper1);
-
-            // Feedback internal
-            List<Feedback> feedbacksIntern1 = new ArrayList<>();
-
-            Feedback feedback1Intern1 = Feedback.builder()
-                    .Level(Level.A2)
-                    .date(LocalDate.of(2024, 1, 01))
-                    .comments("She shows an initial level")
-                    .build();
-
-            Feedback feedback2Intern1 = Feedback.builder()
-                    .Level(Level.A2)
-                    .date(LocalDate.of(2024, 2, 01))
-                    .comments("She does not shows an improvement")
-                    .build();
-
-            Feedback feedback3Intern1 = Feedback.builder()
-                    .Level(Level.B1)
-                    .date(LocalDate.of(2024, 3, 01))
-                    .comments("She has improved")
-                    .build();
-
-            feedbackService.save(feedback1Intern1);
-            feedbackService.save(feedback2Intern1);
-            feedbackService.save(feedback3Intern1);
-
-            feedbacksIntern1.add(feedback1Intern1);
-            feedbacksIntern1.add(feedback2Intern1);
-            feedbacksIntern1.add(feedback3Intern1);
+          
 
             // Attendees
             Attendee attendee1 = attendeeService.save(Attendee.builder()
@@ -94,6 +46,7 @@ public class LoadSampleData {
                     .globalId(12345678)
                     .initialLevel(Level.A2)
                     .status(Status.ENABLE)
+                    .lastLevel(null)
                     .profile(profileService.findById(1))
                     .build());
 
@@ -132,12 +85,68 @@ public class LoadSampleData {
                     .globalId(12345688)
                     .initialLevel(Level.A0)
                     .status(Status.ENABLE)
+                    .lastLevel(null)
                     .profile(profileService.findById(2))
                     .build());
 
-            attendee4.setFeedbacks(feedbacksBootcamper1);
-            attendee5.setFeedbacks(feedbacksIntern1);
+                      // Feedbacks 
+            List<Feedback> feedbacksAttendee1 = new ArrayList<>();
+
+            Feedback feedback1Attendee1 = Feedback.builder()
+                    .Level(Level.C1)
+                    .attendee(attendee1)
+                    .date(LocalDate.of(2024, 1, 01))
+                    .comments("She speaks very well")
+                    .build();
+
+            Feedback feedback2Attendee1 = Feedback.builder()
+                    .Level(Level.C2)
+                    .attendee(attendee1)
+                    .date(LocalDate.of(2024, 2, 01))
+                    .comments("She has improved")
+                    .build();
+
+            feedbackService.save(feedback1Attendee1);
+            feedbackService.save(feedback2Attendee1);
+
+            feedbacksAttendee1.add(feedback1Attendee1);
+            feedbacksAttendee1.add(feedback2Attendee1);
+
+            // Feedback internal
+            List<Feedback> feedbacksAttendee5 = new ArrayList<>();
+
+            Feedback feedback1Attendee5 = Feedback.builder()
+                    .Level(Level.A2)
+                    .attendee(attendee5)
+                    .date(LocalDate.of(2024, 1, 01))
+                    .comments("She shows an initial level")
+                    .build();
+
+            Feedback feedback2Attendee5 = Feedback.builder()
+                    .Level(Level.A2)
+                    .attendee(attendee5)
+                    .date(LocalDate.of(2024, 2, 01))
+                    .comments("She does not shows an improvement")
+                    .build();
+
+            Feedback feedback3Attendee5 = Feedback.builder()
+                    .Level(Level.B1)
+                    .attendee(attendee5)
+                    .date(LocalDate.of(2024, 3, 01))
+                    .comments("She has improved")
+                    .build();
+
+            feedbackService.save(feedback1Attendee5);
+            feedbackService.save(feedback2Attendee5);
+            feedbackService.save(feedback3Attendee5);
+
+            feedbacksAttendee5.add(feedback1Attendee5);
+            feedbacksAttendee5.add(feedback2Attendee5);
+            feedbacksAttendee5.add(feedback3Attendee5);
+            attendee1.setFeedbacks(feedbacksAttendee1);
+            attendee5.setFeedbacks(feedbacksAttendee5);
             attendee5.setLastLevel(attendeeService.getLastLevel(attendee5));
+            attendee1.setLastLevel(attendeeService.getLastLevel(attendee5));
 
 
             // Add correos
