286ec4c87a92fe8f17f9246d83977b27bdaad6ec
cotyarnau <cotyarnau@gmail.com>
2024-02-26 18:26:08 +0100

    Merge

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entities.Attendee;
-import com.example.entities.Status;
 import com.example.services.AttendeeService;
 
 import jakarta.validation.Valid;
@@ -39,56 +38,25 @@ public class MainController {
 
     private final AttendeeService attendeeService;
 
-    // Metodo que devuelve los attendees ENABLE
-    @GetMapping("/enable")
-    public ResponseEntity<List<Attendee>> findByStatusEnable(
+    // Metodo que devuelve los attendees
+    @GetMapping
+    public ResponseEntity<List<Attendee>> findAll(
             @RequestParam(name = "page", required = false) Integer page,
             @RequestParam(name = "size", required = false) Integer size) {
 
         ResponseEntity<List<Attendee>> responseEntity = null;
         Sort sortByName = Sort.by("firstName");
-        List<Attendee> attendeesEnable = new ArrayList<>();
-    
+        List<Attendee> attendees = new ArrayList<>();
+
         // Comprobamos si llega page y size
         if (page != null && size != null) { // si se mete aqui te devuelve los productos paginados
             Pageable pageable = PageRequest.of(page, size, sortByName);
             Page<Attendee> pageAttendees = attendeeService.findAll(pageable);
-            
-            attendeesEnable = pageAttendees.stream().filter(a -> a.getStatus() == Status.ENABLE).collect(Collectors.toList());
-
-            responseEntity = new ResponseEntity<List<Attendee>>(attendeesEnable, HttpStatus.OK);
+            attendees = pageAttendees.getContent();
+            responseEntity = new ResponseEntity<List<Attendee>>(attendees, HttpStatus.OK);
         } else { // solo ordenados alfabeticamente
-            List<Attendee> attendees = attendeeService.findAll(sortByName);
-            attendeesEnable = attendees.stream().filter(a -> a.getStatus() == Status.ENABLE).collect(Collectors.toList());
-
-            responseEntity = new ResponseEntity<List<Attendee>>(attendeesEnable, HttpStatus.OK);
-        }
-
-        return responseEntity;
-    }
-
-    @GetMapping("/disable")
-    public ResponseEntity<List<Attendee>> findByStatusDisable(
-            @RequestParam(name = "page", required = false) Integer page,
-            @RequestParam(name = "size", required = false) Integer size) {
-
-        ResponseEntity<List<Attendee>> responseEntity = null;
-        Sort sortByName = Sort.by("firstName");
-        List<Attendee> attendeesDisable = new ArrayList<>();
-    
-        // Comprobamos si llega page y size
-        if (page != null && size != null) { // si se mete aqui te devuelve los productos paginados
-            Pageable pageable = PageRequest.of(page, size, sortByName);
-            Page<Attendee> pageAttendees = attendeeService.findAll(pageable);
-            
-            attendeesDisable = pageAttendees.stream().filter(a -> a.getStatus() == Status.DISABLE).collect(Collectors.toList());
-
-            responseEntity = new ResponseEntity<List<Attendee>>(attendeesDisable, HttpStatus.OK);
-        } else { // solo ordenados alfabeticamente
-            List<Attendee> attendees = attendeeService.findAll(sortByName);
-            attendeesDisable = attendees.stream().filter(a -> a.getStatus() == Status.DISABLE).collect(Collectors.toList());
-
-            responseEntity = new ResponseEntity<List<Attendee>>(attendeesDisable, HttpStatus.OK);
+            attendees = attendeeService.findAll(sortByName);
+            responseEntity = new ResponseEntity<List<Attendee>>(attendees, HttpStatus.OK);
         }
 
         return responseEntity;
@@ -135,61 +103,103 @@ public class MainController {
 
         return responseEntity;
     }
-
     // Metodo para modificar Attendee por globalId
     @PutMapping("/{globalId}")
-    public ResponseEntity<Map<String, Object>> updateAttendee(@Valid @RequestBody Attendee attendee,
-            BindingResult validationResults, @PathVariable(name = "globalId") Integer globalIdAttendee) {
+    public ResponseEntity<Map<String, Object>> updateAttendee(@Valid @RequestBody Attendee attendee, 
+    BindingResult validationResults, @PathVariable(name = "globalId") Integer globalIdAttendee) {
 
-        Map<String, Object> responseAsMap = new HashMap<>();
-        ResponseEntity<Map<String, Object>> responseEntity = null;
+    Map<String, Object> responseAsMap = new HashMap<>();
+    ResponseEntity<Map<String, Object>> responseEntity = null;
 
-        // Verificar que el attendee existe
-        int idAttendee = attendee.getId();
-        Attendee existingAttendee = attendeeService.findById(idAttendee);
-        if (existingAttendee == null) {
-            String errorMessage = "Attendee with global Id " + attendee.getGlobalId() + " not found";
-            responseAsMap.put("errorMessage", errorMessage);
-            return new ResponseEntity<>(responseAsMap, HttpStatus.NOT_FOUND);
-        }
-
-        // Verificar que el globalId del cuerpo coincida con el globalId del attendee
-        // existente
-        Integer existingAttendeGlobalId = existingAttendee.getGlobalId();
-        if (!globalIdAttendee.equals(existingAttendeGlobalId)) {
-            String errorMessage = "Modification of globalId is not allowed";
-            responseAsMap.put("errorMessage", errorMessage);
-            return new ResponseEntity<>(responseAsMap, HttpStatus.BAD_REQUEST);
-        }
-
-        // Comprobar si el attendee tiene errores
-        if (validationResults.hasErrors()) {
-            List<String> errors = new ArrayList<>();
-
-            List<ObjectError> objectErrors = validationResults.getAllErrors();
-
-            objectErrors.forEach(objectError -> errors.add(objectError.getDefaultMessage()));
-
-            responseAsMap.put("errors", errors);
-            responseAsMap.put("Attendee Error", attendee);
-
-            return new ResponseEntity<>(responseAsMap, HttpStatus.BAD_REQUEST);
-        }
-
-        // Actualizar el Attendee
-        try {
-            Attendee attendeeUpdate = attendeeService.save(attendee);
-            String succesMessage = "The attendee has been saved succesfully";
-            responseAsMap.put("Succes Message", succesMessage);
-            responseAsMap.put("Attendee update", attendeeUpdate);
-            responseEntity = new ResponseEntity<>(responseAsMap, HttpStatus.OK);
-        } catch (DataAccessException e) {
-            String error = "Error updating the attendee: " + e.getMostSpecificCause();
-            responseAsMap.put("error", error);
-            responseAsMap.put("The attende has attempted to update", attendee);
-            responseEntity = new ResponseEntity<>(responseAsMap, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-        return responseEntity;
+    // Verificar que el attendee existe
+    int idAttendee = attendee.getId();
+    Attendee existingAttendee = attendeeService.findById(idAttendee);
+    if (existingAttendee == null) {
+        String errorMessage = "Attendee with global Id " + attendee.getGlobalId() + " not found";
+        responseAsMap.put("errorMessage", errorMessage);
+        return new ResponseEntity<>(responseAsMap, HttpStatus.NOT_FOUND);
     }
+
+    // Verificar que el globalId del cuerpo coincida con el globalId del attendee existente
+    Integer attendeGlobalId = attendee.getGlobalId();
+    Integer existingAttendeGlobalId = existingAttendee.getGlobalId();
+    if (!attendeGlobalId.equals(existingAttendeGlobalId)) {
+        String errorMessage = "Modification of globalId is not allowed";
+        responseAsMap.put("errorMessage", errorMessage);
+        return new ResponseEntity<>(responseAsMap, HttpStatus.BAD_REQUEST);
+    }
+
+    // Comprobar si el attendee tiene errores
+    if (validationResults.hasErrors()) {
+        List<String> errors = new ArrayList<>();
+        
+        List<ObjectError> objectErrors = validationResults.getAllErrors();
+
+        objectErrors.forEach(objectError -> errors.add(objectError.getDefaultMessage()));
+
+        responseAsMap.put("errors", errors);
+        responseAsMap.put("Attendee Error", attendee);
+
+        return new ResponseEntity<>(responseAsMap, HttpStatus.BAD_REQUEST);
+    }
+
+    // Actualizar el Attendee
+    try {
+        Attendee attendeeUpdate = attendeeService.save(attendee);
+        String succesMessage = "The attendee has been saved succesfully";
+        responseAsMap.put("Succes Message", succesMessage);
+        responseAsMap.put("Attendee update", attendeeUpdate);
+        responseEntity = new ResponseEntity<>(responseAsMap, HttpStatus.OK);
+    } catch (DataAccessException e) {
+        String error = "Error updating the attendee: " + e.getMostSpecificCause();
+        responseAsMap.put("error", error);
+        responseAsMap.put("The attende has attempted to update", attendee);
+        responseEntity = new ResponseEntity<>(responseAsMap, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    return responseEntity;
 }
+
+
+// @GetMapping("/{globalId}")
+// public ResponseEntity<Attendee> getAttendeeById(@Valid @RequestBody Attendee attendee, 
+//  @PathVariable(name = "globalId") Integer globalIdAttendee) {
+
+//      int idAttendee = attendee.getId();
+//      Attendee thisAttendee = attendeeService.findById(idAttendee);
+//      thisAttendee= thisAttendee.builder()
+//      .firstName(thisAttendee.getFirstName())
+//      .surname(thisAttendee.getSurname())
+//      .emails(thisAttendee.getEmails())
+//   .profile(thisAttendee.getProfile())
+//   .feedbacks(thisAttendee.getFeedbacks())
+//   .initialLevel(thisAttendee.getInitialLevel())
+//   .globalId(thisAttendee.getGlobalId())
+//   .build();
+  
+      
+  
+//   return new ResponseEntity<>(thisAttendee, HttpStatus.OK);
+// }
+
+@GetMapping("/{globalId}")
+public ResponseEntity<Attendee> getAttendeeById(@PathVariable(name = "globalId") Integer globalIdAttendee) {
+    Attendee attendee = attendeeService.findById(globalIdAttendee);
+    
+    if (attendee == null) {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    // Crear un objeto de respuesta con solo los campos necesarios
+    Attendee thisAttendee = new Attendee();
+    thisAttendee.setFirstName(attendee.getFirstName());
+    thisAttendee.setSurname(attendee.getSurname());
+    thisAttendee.setId(attendee.getId());
+    thisAttendee.setEmails(attendee.getEmails());
+
+    return new ResponseEntity<Attendee> (thisAttendee, HttpStatus.OK);
+}
+
+}
+
+
